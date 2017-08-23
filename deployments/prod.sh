ec2131e9b7f0d44b45cb3d37845fc50b0ad14366
Santiago Jimenez <sago2k8@gmail.com>
2017-08-23 15:43:09 -0500

    fix errors

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,4 +1,4 @@
 wget -O packer.zip https://releases.hashicorp.com/packer/1.0.4/packer_1.0.4_linux_amd64.zip?_ga=2.243382213.1575990318.1503506122-355694350.1503506122
 unzip packer.zip
-./packer validate /deployments/template.json
-./packer build /deployments/template.json
+./packer validate deployments/template.json
+./packer build deployments/template.json
