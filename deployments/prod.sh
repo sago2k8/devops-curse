0a4c57b23fa85dc1b9de5fa4976d937c0c26afef
Santiago Jimenez <sago2k8@gmail.com>
2017-08-23 16:47:29 -0500

    fix issue

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -2,7 +2,7 @@ wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.0.4/packer_1.0.4
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.10.2/terraform_0.10.2_linux_amd64.zip?_ga=2.7796338.197602616.1503508734-129106718.1503508734
 unzip /tmp/packer.zip -d ~/bin
 unzip /tmp/terraform.zip -d ~/bin
-packer validate deployments/template.json && ./packer build deployments/template.json
+packer validate deployments/template.json && packer build deployments/template.json
 export TF_VAR_image_id=$(curl -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" https://api.digitalocean.com/v2/images?private=true | jq ."images[] | select(.name == \"platzi-demo-$CIRCLE_BUILD_NUM\") | .id")
 echo $TF_VAR_image_id
 cd infra && terraform apply && cd .. &&
